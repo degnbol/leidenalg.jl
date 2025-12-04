76a5339f03e8378f185ab3f91994b8041f5cd6af
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2025-12-04 12:47:38 +0000

    lazy install python deps

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Leiden algorithm wrapper in Julia.
 Wraps the Python package `leidenalg`, 
 which itself is a wrapper of C++ code, see
 https://github.com/vtraag/leidenalg.
-Calls Python code from Julia using `PythonCall`.
+Calls Python code from Julia using `PyCall`.
 
 Example use:
 ```julia
@@ -13,4 +13,3 @@ adj = randn(7, 7)
 partitions = leiden(adj)
 @assert size(partitions) == (7,)
 ```
-
--- src/Leiden.jl
+++ src/Leiden.jl
@@ -1,14 +1,27 @@
 module Leiden
 export leiden
 
-include("pythoncall.jl") # pyinclude
+# Lazy install python deps to local env.
+env = joinpath(@__DIR__(),  "..", ".venv")
+ENV["PYTHON"] = joinpath(env, "bin", "python")
+ENV["PYCALL_JL_RUNTIME_PYTHON"] = ENV["PYTHON"]
+ENV["PYTHONPATH"] = @__DIR__()
+if !isdir(env)
+    print("Installing python dependencies...")
+    mkdir(env)
+    using Conda
+    Conda.create(env)
+    Conda.add(["numpy", "leidenalg"], env)
+    using Pkg
+    Pkg.build("PyCall")
+end
 
-py_leiden, = pyinclude(@__DIR__() * "/leiden.py", :leiden)
+using PyCall
 
-function leiden(adjacency_weighted::T) where T<:AbstractMatrix
-    py_mat = Py(adjacency_weighted).to_numpy()
-    py_comm = py_leiden(py_mat)
-    pyconvert(Vector, py_comm)
+@pyinclude(joinpath(@__DIR__(), "leiden.py"))
+
+function leiden(adj::T) where T<:AbstractMatrix
+    py"leiden"(adj)
 end
 
 end;
